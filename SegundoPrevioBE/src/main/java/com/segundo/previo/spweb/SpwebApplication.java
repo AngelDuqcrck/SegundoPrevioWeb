3cfb1bd3bb9b438359cd12da06ac8705820daaaf
Angel Yesid Duque <angelyesidducr@ufps.edu.co>
2023-11-22 17:59:54 -0500

    First Commit: Init project

---
+++ SegundoPrevioBE/src/main/java/com/segundo/previo/spweb/SpwebApplication.java
@@ -0,0 +1,13 @@
+package com.segundo.previo.spweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpwebApplication.class, args);
+	}
+
+}
---
+++ SegundoPrevioBE/src/test/java/com/segundo/previo/spweb/SpwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segundo.previo.spweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
